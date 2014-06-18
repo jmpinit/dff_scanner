6302318e3d581a53360f7f8ef26fce01f414e047
Owen Trueblood <jmptable@mit.edu>
2014-06-18 22:11:52 +0000

    Depth taken to be index of most focused layer.

--- depthmap/src/depthmap/core.clj
+++ depthmap/src/depthmap/core.clj
@@ -15,9 +15,22 @@
 
 (defn gaussian
   [theta x]
-  (let [[a b c d] theta
+  (let [[a b c] theta
         inner (* -1 (/ (Math/pow (- x b) 2) (* 2 (* c c))))]
-    (+ d (* a (Math/exp inner)))))
+    (* a (Math/exp inner))))
+
+(defn fit-gaussian
+  "Fits a gaussian curve to a set of points. Returns the curve's parameters."
+  [xs ys]
+  (let [logged (map #(Math/log %) ys)
+        a 1 ; TODO
+        b 2
+        c 3]
+    [a b c]))
+
+(defn max-gaussian
+  [a b c]
+  b)
 
 (defn clean-image
   "Remove incompatible data to work around image operation bugs in ConvolveOp."
@@ -51,15 +64,30 @@
     (convolve image laplacian)))
 
 (defn to-rgb
-  "Converts intensity 0-255 to an int-encoded RGB value."
-  [intensity]
-  (java.awt.Color/HSBtoRGB 0 0 intensity))
+  "Encodes red, green, and blue into an integer."
+  [r g b]
+  (let [s #(bit-shift-left (bit-and %1 0xFF) %2)]
+    (bit-or (s r 16) (s g 8) b)))
+
+(defn rgb-to-intensity
+  "Intensity of an int-encoded RGB value."
+  [rgb]
+  (let [r (bit-shift-right (bit-and rgb 0xFF0000) 16)
+        g (bit-shift-right (bit-and rgb 0xFF00) 8)
+        b (bit-and rgb 0xFF)]
+    (float (div (+ r g b) 3))))
+
+(defn argmax
+  "Index of maximum value in sequence."
+  [vs]
+  (first (apply max-key second (map-indexed vector vs))))
 
 (defn main
   [& filenames]
   (if (= (count filenames) 0)
     (println "Must specify at least one image filename.")
     (let [src-images (map load-an-image filenames)
+          z-resolution (int (div 255 (count src-images)))
           width (.getWidth (first src-images))
           height (.getHeight (first src-images))
           num-pixels (* width height)
@@ -67,17 +95,25 @@
           ys (range height)
           coordinates (cart [xs ys])
           focus-images (map convolve-laplacian src-images)
+          ;_ (do (def plot (scatter-plot))
+          ;      (view plot))
           depth-map (map (fn [[x y]]
-                           (let [intensities (map #(.getRGB % x y) focus-images)
-                                 zs (range (count intensities))
-                                 start [1.0 0.0 1.0 0.0]
-                                 depth (non-linear-model gaussian intensities zs start)]
+                           (let [intensities (map #(rgb-to-intensity (.getRGB % x y)) focus-images)
+                                 ;zs (range (count intensities))
+                                 ;_ (let [smallest (apply min intensities)
+                                 ;      normed (map #(- % smallest) intensities)]
+                                 ;  (add-lines plot zs normed))
+                                 ;focus-curve (fit-gaussian zs intensities)
+                                 ;depth (apply max-gaussian focus-curve)]
+                                 depth (argmax intensities)]
                              [[x y] depth]))
             coordinates)
           blank-depth-image (BufferedImage. width height BufferedImage/TYPE_INT_RGB)
-          depth-image (map (fn [[x y] depth]
-                             (.setRGB blank-depth-image x y (to-rgb depth)))
-                           depth-map)]
-    ; display depth map
-    (count depth-map)
-    (show blank-depth-image))))
+          depth-image (doall (map (fn [[[x y] depth]]
+                                    (let [dc (* depth z-resolution)]
+                                          ;_ (println (format "%x" (to-rgb dc dc dc)))]
+                                      (.setRGB blank-depth-image x y (to-rgb dc dc dc))))
+                                  depth-map))]
+      ; display depth map
+    (show blank-depth-image)
+    )))
